1375f46a49bbbb02b360c11fd900f290c4d97a69
CamilleCHIQUET <chiquetcamille@gmail.com>
2020-01-03 16:15:23 +0100

    first commit

---
+++ 2_train_on_data.py
@@ -0,0 +1,16 @@
+import multiprocessing as mp
+import os
+import sys
+
+_PATH_ = os.path.dirname(os.path.dirname(__file__))
+
+if _PATH_ not in sys.path:
+	sys.path.append(_PATH_)
+
+if __name__ == "__main__":
+	mp.set_start_method('spawn')
+	sys.setrecursionlimit(10000)
+	import manager
+
+	args = {"type": "normal", "cmd": "opt", "total_step": 10}
+	manager.start(args)
