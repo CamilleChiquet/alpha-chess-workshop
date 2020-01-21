4a6a0674f7745ed339c539ea4bd317eb46558069
Camille Chiquet <chiquetcamille@gmail.com>
2020-01-21 15:46:24 +0100

    Script to run bigger networks

--- 2_supervised_learning.py
+++ 2_supervised_learning.py
@@ -4,8 +4,17 @@ Supervised Learning on generated training data.
 import multiprocessing as mp
 import os
 import sys
+import keras.backend as K
 
 _PATH_ = os.path.dirname(os.path.dirname(__file__))
+
+import memory_saving_gradients
+"""
+Uncomment the following line if your model doesn't fit in your gpu memory.
+Training will be a bit slower but you will be able to use bigger networks.
+To learn more about : https://medium.com/tensorflow/fitting-larger-networks-into-memory-583e3c758ff9
+"""
+# K.__dict__["gradients"] = memory_saving_gradients.gradients_memory
 
 if _PATH_ not in sys.path:
 	sys.path.append(_PATH_)
