f15625a6558793e4b4ec4cae76d58979fa5feff6
Camille Chiquet <chiquetcamille@gmail.com>
2020-02-07 17:17:45 +0100

    Description update

--- test_algo_against_other.py
+++ test_algo_against_other.py
@@ -1,8 +1,5 @@
 """
-Make the network play against itself and records the games inside a game.pgn file.
-
-The "data/model/model_best_weight.h5" model will be loaded. Make sure it is the one you want to use.
-If it doesn't exist, a model with random weights and biases will be used to play.
+Make to networks compete against each other.
 """
 import multiprocessing as mp
 import os
@@ -18,8 +15,8 @@ if __name__ == "__main__":
 	sys.setrecursionlimit(10000)
 	import manager
 
-	white_model_path = "data/model/new"
+	white_model_path = "data/model/last"
 	black_model_path = "data/model/old"
 	# model_1 is white, model_2 plays as black player
 	manager.start(worker="duel", config_type="normal", model_1_path=white_model_path, model_2_path=black_model_path,
-	              deterministic=True)
+	              deterministic=False)
